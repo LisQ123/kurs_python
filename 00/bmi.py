e496f754d890986ecd613feb7e406d04bdd60af6
Daria <dariadec.96@gmail.com>
2019-10-09 15:30:54 +0200

    cwiczenia

---
+++ 00/bmi.py
@@ -0,0 +1,5 @@
+weight = input("Podaj wagę w kg: ")
+height = input("Podaj wzrost w metrach: ")
+
+BMI = weight / ( height ** 2 )
+print("Twoje BMI wynosi", BMI)
--- 00/weeks.py
+++ 00/weeks.py
@@ -1,4 +1,4 @@
 czas = 7*7*24*60
 print("Zadanie 1. Liczba minut w ciągu siedmiu tygodni wynosi: ", czas)
-time =
-print("Zadanie 2. Zdobycie nowej umiejętności zajmie mi:", )
+time = 75/5
+print("Zadanie 2. Zdobycie nowej umiejętności zajmie mi:", time, "tygodni w założeniu, że będę się uczyć przez 5h w ciągu tygodnia." )
