4c39413396b3f406c2da5d67c58cc4de76137faf
Daria D <56266918+LisQ123@users.noreply.github.com>
2019-10-19 00:12:10 +0200

    Add files via upload

---
+++ 02/petla_for_zad2.py
@@ -0,0 +1,5 @@
+components = ["bread.", "butter.", "ham."]
+action = ["Cut the", "Use", "Put the"]
+
+for i in range(len(components)):
+    print(action[i], components[i])
---
+++ 02/petla_while_zad2.py
@@ -0,0 +1,7 @@
+secret_num = 7
+number = int(input("Let's play a game! Give me number 1-20: "))
+
+while number != secret_num:
+        print("Lose!")  #
+        number = int(input("Give me another number 1-20:"))
+print("Win!")
