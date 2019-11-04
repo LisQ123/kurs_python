92ecb327330a0aa1fe945388202ae9e068867f95
Daria D <56266918+LisQ123@users.noreply.github.com>
2019-11-04 16:23:28 +0100

    Add files via upload

---
+++ script.py
@@ -0,0 +1,12 @@
+import figury as f
+import test
+
+a = 5
+b = 8
+h = 4
+
+print(f.triangle_field(a, b))
+print(f.diamond_field(b, h))
+print("Pole kwadratu wynosi:", f.square_field(b))
+
+print(__name__)
