41aa316817637af0b1a083ce74b49167cd73590a
Daria D <56266918+LisQ123@users.noreply.github.com>
2019-11-05 23:22:04 +0100

    Add files via upload

---
+++ 07/zad_5/pola_figur.py
@@ -0,0 +1,23 @@
+def rectangle_field(base, side):
+    result = base * side
+    return result
+
+
+def triangle_field(base, height):
+    result = 0.5 * base * height
+    return result
+
+
+def square_field(base):
+    result = base ** 2
+    return result
+
+
+def diamond_field(base, height):
+    result = base * height
+    return result
+
+
+def trapeze_field(base, side, height):
+    result = ((base + side) * height)/2
+    return result
